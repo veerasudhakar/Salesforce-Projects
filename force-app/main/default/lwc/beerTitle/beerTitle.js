4cb6f8fa8e666da10afe6e450856a227be2702f0
sukku <sukanyam425@gmail.com>
2022-05-08 21:04:55 +0530

    added changed

--- force-app/main/default/lwc/beerTitle/beerTitle.js
+++
@@ -1,7 +0,0 @@
-import { LightningElement,track,api } from 'lwc';
-import beer from '@salesforce/resourceUrl/beer';
-export default class BeerTitle extends LightningElement {
-    @api beerRecord;
-    @track beerImg = beer;
-    
-}
